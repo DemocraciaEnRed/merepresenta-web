7b483127e7e3297cf52e4ab98abc9e6938a26b58
mijail cohen <mijaelcohen@gmail.com>
2021-09-02 16:37:44 -0300

    basic shit for proposal by subject

---
+++ src/routes/plataformas/[provincia]/temas/[tema]/index.svelte
@@ -0,0 +1,42 @@
+<script context="module">
+  export function load({fetch, page}){
+    return {
+      props: {}
+    }
+  }
+</script>
+<script>
+  import { page } from '$app/stores';
+  import propuestas from '$lib/proposal-x-partido.json';
+  let routePath;
+  page.subscribe(({path})=>{
+    routePath = path
+  })
+</script>
+<section>
+  {#each propuestas as proposal}
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-48x48">
+              <img src={proposal.party.logo} class="is-rounded" alt="{proposal.party.name} logo">
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">{proposal.party.name}</p>
+          </div>
+        </div>
+      </div>
+      {#each proposal.items as item}
+        <div class="propuesta">
+          <h2>{item.title}</h2>
+          <p>{item.intro}</p>
+          <p class="has-text-right">
+            <a class="button" href="{routePath}/propuestas/{item.slug}">Leer más</a>
+          </p>
+        </div>
+      {/each}
+    </div>
+  {/each}
+</section>
--- src/routes/plataformas/[provincia]/temas/index.svelte
+++ src/routes/plataformas/[provincia]/temas/index.svelte
@@ -1,6 +1,40 @@
 <script>
   import { page } from "$app/stores";
-  const { provincia } = $page.params;
-  // get partidos by provincia
+  import Icon from "$lib/common/Icon.svelte";
+  import provincias from '$lib/provincias.json';
+  // get temas
+  import temas from '$lib/temas.json';
+  let path;
+  let district;
+  page.subscribe((currentValue)=>{
+    path = currentValue.path
+    district = provincias.find((p)=>{return p.slug === currentValue.params.provincia})
+  })
+  
 </script>
-<h1 class="has-text-center">Plataformas electorales en {provincia}</h1>
+<h1 class="has-text-center">Plataformas electorales por tema en {district.name}</h1>
+<h2>Conocé los temas que abordan las distintas propuestas</h2>
+<div class="container">
+  <section class="columns mt-6">
+    {#each temas as tema}
+      <a class="column tema" href="{path}/{tema.slug}" >
+        <Icon size="large" icon={tema.icon} />
+        <br/>
+        {tema.name}
+      </a>
+    {/each}
+  </section>
+  
+</div>
+<style>
+  .tema{
+    width: 50%;
+    float: left;
+    text-align: center;
+    padding: 10px;
+    align-content: center;
+    height: 100px;
+    border: 4px solid white;
+    background: #ccc;
+  }
+</style>
