d1890ef54dc9f1cd5b0332dc8339470c772bd976
mijail cohen <mijaelcohen@gmail.com>
2021-09-21 18:09:50 -0300

    test deploy

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,10 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter({
+		paths: {
 			base: '/merepresenta-web',
-		})
+		},
+		adapter: adapter()
 	},
 };
 
