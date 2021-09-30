c3711541f166da0addf0f66b68680331c7ff821e
mijail cohen <mijaelcohen@gmail.com>
2021-09-30 18:20:14 -0300

    update home buttons

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,8 +14,20 @@
 	<div class="has-text-centered">
 		<section class="container p-4">
 			<h2 class="title is-4 has-text-white has-text-weight-normal">A vos, ¿quién te representa?</h2>
-			<a href="/partidos-y-candidates/donde-votas" class="button is-medium is-fullwidth is-uppercase mb-4">ver partidos y candidates</a>
-			<a href="/plataformas/donde-votas" class="button transparent is-medium is-fullwidth is-uppercase ">ver propuestas</a>
+			<div class="has-text-centered mt-6 mb-2">
+				<img src="/candidates-link.svg" alt="">
+			</div>
+			
+			<a href="/partidos-y-candidates/donde-votas" class="button py-5 is-fullwidth is-uppercase mb-4">
+				Conocé todo sobre los <br> <strong>partidos y lxs candidatxs</strong>
+			</a>
+			<div class="has-text-centered mt-6 mb-2">
+				<img src="/proposals-link.svg" alt="">
+			</div>
+			<a href="/plataformas/donde-votas" class="button py-5 transparent is-fullwidth is-uppercase">
+				enterate de todas sus  <br>
+				<strong>propuestas</strong>
+			</a>
 		</section>
 		<section class="container p-4 ">
 			<h2 class="title light is-4 has-text-weight-normal has-text-white">¿No sabés qué se vota?</h2>
@@ -24,4 +36,11 @@
 	</div>
 </main>
 <style>
+	.button{
+		display: flex;
+		border: 1px solid white;
+		flex-direction: column;
+		border-radius: 10px;
+	}
+
 </style>
