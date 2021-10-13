a8844b174bbc7ee7b5bad9699af1c89d6d70221d
Guillermo Croppi <guillermocroppi@gmail.com>
2021-10-13 12:54:54 -0300

    Como votaron

--- src/routes/como-votaron/index.svelte
+++ src/routes/como-votaron/index.svelte
@@ -3,22 +3,39 @@
   import { getLaws } from '$lib/graph-ql/laws';
   //load es un hook de svelte cuando carga la ruta
   export async function load({fetch}){
-    const res = API(fetch, getLaws);
+    const res = await API(fetch, getLaws);
     // handle response devuelve o error o props
     return handleResponse(res, "laws", "leyes");
+    // const {party, tema} = page.params;
+    // const res = await API(fetch ,getThemeProposalsByPartyId(tema, party))
+    // return await handleResponse(res, "party", "partido");
   }
 </script>
 <script>
   import Dropdown from "$lib/common/Dropdown.svelte";
+  import { directusImg } from "$lib/common/utils";
   export let laws;
-  import Votes from './_votes.svelte';
+  import Votes from './_votes.svelte.delete';
 </script>
-<main class="container p-2 has-background-black has-text-centered">
-  <img src="/abc.svg" class="mt-4" alt="imagen de una mano deslizando un voto en una urna">
-  <h1 class="title is-5 my-4 p-4 has-text-weight-normal has-text-white">Conocé cómo votó cada uno de los partidos en las siguientes leyes</h1>
-</main>
-{#each laws as law}
-  <Dropdown  name={law.name.toUpperCase()}>
-    <Votes votes={law.votes} />
-  </Dropdown>
-{/each}
+<div class="section has-background-black has-text-centered">
+  <div class="container">
+    <img src="/abc.svg" alt="imagen de una mano deslizando un voto en una urna">
+    <h1 class="title is-5 has-text-weight-normal has-text-white">Conocé cómo votó cada uno de los partidos en las siguientes leyes</h1>
+  </div>
+</div>
+<div class="section">
+  <div class="container">
+    {#each laws as law}
+      <Dropdown  name={`(${law.year}) ${law.title.toUpperCase()}`}>
+        <img src="{directusImg}{law.infografia.id}" class="image" width="500" alt="">
+      </Dropdown>
+    {/each}
+  </div>
+</div>
+
+ <!-- {#each laws as law}
+  <div class="column is-4">
+    <h1 class="title is-4">{law.title.toUpperCase()} ({law.year})</h1>
+    <img src="{directusImg}{law.infografia.id}" class="image" width="500" alt="">
+  </div>
+{/each} -->
