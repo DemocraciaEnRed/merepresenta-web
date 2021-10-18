dde17f6902a63b553df7776ea977730c32d953c6
lieta96 <julieta.garcia96@gmai.com>
2021-10-18 12:15:11 -0300

    ajustes logos

--- src/lib/footer/index.svelte
+++ src/lib/footer/index.svelte
@@ -4,14 +4,17 @@
 <footer class="footer has-background-{backgroundColor}">
     <div class="container">
         <div class="columns invert-mobile">
-            <div class="column is-narrow has-text-centered-touch">
-                <img src="/logo-merepresenta.svg" class="image mx-auto {backgroundColor === 'black' ? null : 'logo-mere'}" width="300" alt="Logo de Me Representa">
-                <p class="has-text-{backgroundColor === 'black' ? 'white' : 'black'} mt-5">Desarrollado con ❤️ por</p>
-                <img src="/logo-der-{backgroundColor === 'black' ? 'white' : 'negro'}.svg" width="150"  class="image logo-der" alt="Logo de Democracia en Red">
+            <div class="column is-half has-text-centered-touch is-flex is-align-items-flex-start">
+                <img src="/logo-merepresenta.svg" class="image mx-auto {backgroundColor === 'black' ? null : 'logo-mere'}" width="200" alt="Logo de Me Representa">
+                <div>
+                    <p class="has-text-{backgroundColor === 'black' ? 'white' : 'black'} ">Desarrollado con ❤️ por</p>
+                <img src="/logo-der-{backgroundColor === 'black' ? 'white' : 'negro'}.svg" width="100"  class="image logo-der" alt="Logo de Democracia en Red">
+                </div>
+                
             
             </div>
             <div class="column is-offset-1 has-text-centered-touch">
-                <h3 class="general-sans  is-size-7 {backgroundColor === 'black' ? 'has-text-white has-text-weight-normal' : 'has-text-weight-normal has-text-black'}" >Si encontrás algún dato incorrecto o querés sumar tu aporte,
+                <h3 class="general-sans  is-size-6 {backgroundColor === 'black' ? 'has-text-white has-text-weight-normal' : 'has-text-weight-normal has-text-black'}" >Si encontrás algún dato incorrecto o querés sumar tu aporte,
                     podés contactarnos en: <a class="general-sans has-text-weight-semibold {backgroundColor === 'black' ? 'has-text-white' : 'has-text-black'}" href="mailto:contacto@democracyos.io">contacto@democraciaenred.org</a>
                 </h3>
             </div>
