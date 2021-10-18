bde34edb69bbde1decf2efcd73ff0a809e33e9e9
lieta96 <julieta.garcia96@gmai.com>
2021-10-18 11:14:15 -0300

    New section "acerca de"

---
+++ src/routes/acerca-de.svelte
@@ -0,0 +1,138 @@
+<main>
+    <div class="hero is-black is-halfheight tetris-background">
+		<div class="hero-body">
+			<div class="container">
+				<img src="/logo-merepresenta.svg" width="400" class="image p-4 mb-5 is-hidden-desktop" alt="Logo de Me Representa">
+				<div class="columns is-centered is-mobile">
+					<div class="column is-8-desktop">
+						<h1 class="title is-3 is-size-4-touch has-text-centered has-text-left-touch has-text-white mb-6">
+							Acerca de
+						</h1>
+						<p class="subtitle is-3 is-size-4-touch has-text-centered has-text-left-touch has-text-white is-size-4"><strong></strong>
+							<strong>REPRESENTAR</strong> es <strong>personificar</strong>, es “actuar en nombre de”. En política, REPRESENTAR tiene que ver con exponer <strong>deseos, solidaridades, intereses</strong> y necesidades de una sociedad. En este 2021, vamos a votar <strong>representantes para el Congreso Nacional.</strong> <br>
+                            <strong>Las elecciones no son concursos de popularidad, se tratan de dar responsabilidades.</strong> Para elegir algo tan importante son indispensables los <strong>detalles personales de las y los candidatxs,</strong> pero también es necesario que sus <strong>propuestas</strong> sean <strong>conocidas</strong> y que puedan ser <strong>discutidas</strong>. <br>
+                            Porque si <strong>tus representantes van a representarte</strong>, te tienen que explicar <strong>quiénes son y qué han hecho</strong>. Si van a actuar en tu nombre, te tienen que explicar <strong>qué quieren hacer y cómo.</strong> 
+						</p>
+					</div>
+				</div>
+			</div>
+        </div>
+    </div>
+    <section calss="has-background-white" >
+        <div class="container py-6 px-2">
+            <h1 class="title is-3 is-size-4-touch has-text-centered has-text-left-touch has-text-white mb-6 has-text-black">
+                ¿Quiénes somos?
+            </h1>
+            <p class="subtitle is-3 is-size-4-touch has-text-centered has-text-left-touch has-text-white is-size-4 has-text-black">
+                Somos organizaciones de la sociedad civil que nos unimos para crear <strong>#MeRepresenta</strong>, una plataforma simple, atractiva e interactiva donde la ciudadanía pueda informarse sobre candidaturas, partidos, propuestas y, también, aprender más sobre las elecciones en general. <br>
+                <strong>#MeRepresenta</strong> tiene como objetivo ayudarte a pensar tu voto y contribuir así a una campaña menos centrada en las descalificaciones personales y más orientada a las propuestas.
+            </p>
+            <div class="is-flex is-justify-content-space-evenly py-3">
+                <img src="/logo-merepresenta.svg" alt="Logo de Me representa" width=250 class="logo-mere">
+                <img src="/logo-der-negro.svg" alt="Logo de Democracia en Red" width=200>
+
+            </div>
+        </div>
+        <div class="container py-6 px-2">
+            <h1 class="title is-3 is-size-4-touch has-text-centered has-text-left-touch has-text-white mb-6 has-text-black is-size-3">
+                Nos apoyan:
+            </h1>
+            <div class="is-flex is-justify-content-space-evenly py-3">
+                <img src="/logo-avina.svg" alt="Logo de Me representa" width=250 >
+                <img src="/twitter.svg" alt="Logo de Democracia en Red" width=100>
+
+            </div>
+        </div>
+        <div class="container py-6 has-text-centered has-text-left-touch is-size-4 px-2">
+            <h1 class="title is-3 is-size-3 has-text-centered has-text-left-touch has-text-white mb-6 has-text-black">
+                Créditos
+            </h1>
+            <p class="subtitle is-3 is-size-4-touch has-text-centered has-text-left-touch has-text-white is-size-4 has-text-black">
+                Las secciones de #MeRepresenta fueron desarrolladas, en conjunto, entre diferentes organizaciones
+            </p>
+            <p class="mb-3"><strong>ABC ELECTORAL </strong>
+                 <br>
+                Poder Ciudadano + CIPPEC + Democracia en Red
+            </p>
+            <p class="mb-3">
+                <strong>CANDIDATURAS y PARTIDOS</strong>
+                <br>
+                Observatorio de Redes + Democracia en Red
+            </p>
+            <p class="mb-3">
+                <strong>PROPUESTAS</strong>
+                <br>ACIJ + Nuestra Córdoba + EcoFeminita + Conocimiento Abierto + Democracia en Red
+
+            </p>
+            <p class="mb-3">
+                <strong>TRIVIA</strong>
+                <br>Directorio Legislativo + Democracia en Red
+            </p>
+            <p class="mb-3">
+                <strong>COMPÁS POLÍTICO</strong>
+                <br>Democracia en Red <br>
+                Idea: Antonio Milanese + Agustin Frizzera
+            </p>
+            <p class="mb-3">
+                <strong>VERDADERO - FALSO ELECTORAL (próximamente)</strong>
+                <br>Poder Ciudadano + Democracia en Red
+            </p>
+            <p class="mb-3">
+                <strong>JUEGO DE LA OCA en CAMPAÑA (próximamente)</strong>
+                <br>Poder Ciudadano + Democracia en Red
+            </p>
+            <p class="mb-3">
+                <strong>Coordinación general del proyecto:</strong> Democracia en Red
+            </p>
+        </div>
+        <div  class="container py-6 has-text-centered has-text-left-touch is-size-4 px-2">
+            <h1 class="title is-3 has-text-centered has-text-left-touch has-text-white mb-6 has-text-black is-size-3">
+                Agradecimientos
+            </h1>
+            <p class="mb-4">
+                Este  sitio fue hecho por mucha gente. La gran mayoría, personas que pusieron su compromiso, profesionalismo y esfuerzo sólo a cambio de este “gracias”. 
+            </p>
+            <p>
+                A Guada y Juani, del Observatorio de Redes, surcaron horizontes llenos de datos, los delimitaron, los estructuraron y los vincularon con el repositorio de <a href="https://politicosentwitter.com/" target="_blank">Politcxs en Twitter</a> ;) 
+            </p>
+            <p class="mb-4">
+                A Karina y a Pablo, de Poder Ciudadano, por su sabiduría, su velocidad y su sentido del humor. 
+
+            </p>
+            <p class="mb-4">
+                A Juan y a Noel, de Directorio Legislativo, por colaborar con las preguntas de la Trivia. A Mechi y a Agus por la idea original: ¿quién dijo “nunca es tarde para refritar buenas ideas”? 
+
+            </p>
+            <p class="mb-4">
+                A Gonzalo y a Manuel, de CIPPEC, por compartir su excelente material.  A Iván y a Chani, de Redacción, por el acompañamiento, porque necesitamos nuevas formas de comunicar.  A Nina, de FUNDEPS, por estar. 
+
+            </p>
+            <p class="mb-4">Al Ruso, por sus 93 minutos, cada uno fue valioso: aprendemos de vos. A Toni, por el juego y el Compás Político: lo hicimos con las categorías que quisimos.  
+            </p>
+            <p>A quienes pusieron horas editando las propuestas, Macarena y Emilse, de Nuestra Córdoba. A quienes le pusieron criterios a los diagnósticos , a Emilia (EcoFeminita), a Ornella y equipo (Acción Colectiva), a Carmen (Huésped) y a Joaco, RO !  y equipo de ACIJ, 
+            </p>
+            <p class="mb-4">A voluntarias y voluntarios: Camila Roberto, Carolina Diez, Agustin Polzella, Federico Bertero, Agostina Amirante, Camilo Parra, Facundo Bustos, Candelaria Figueredo, Melanie Gobetto, Guillermo Fernández, Ailen Diaz Testa, Nicolás Galvan, Greta Liporace, Lucas Abeldaño, Lucas Espiñeira, Lourdes Cabrera e Isabella Tubelli. Con gran esfuerzo, compromiso y dedicación ayudaron a completar las bases de datos de candidaturas, partidos y propuestas electorales. 
+            </p>
+            <p class="mb-4">A Lucia, a Sara y a Tarick, de Avina, por la confianza y el acompañamiento en todas las etapas, por esas ideas que nos reafirman en la búsqueda de nuevas maneras de comunicar.
+
+            </p>
+            <p class="mb-4">Al equipo de Democracia en Red.</p>
+            <p class="mb-4">Si la navegación de kichicientos datos es lo más simple posible es por la cabeza de Antonella Meduri y Federico Pasutti. Si las cosas se ven bien, si el mensaje está ajustado (y aprobado) es gracias a Mauro Salerno, Agustin Rabinovich, Leomar Solorzano, Daniel Zárate, Alejandra González y Nicole Alonso. A Agus Manuele, por la magia. 
+            </p>
+            <p class="mb-4">A Mijail Cohen y Julieta García que codearon la plataforma, día y noche (está disponible en código abierto <a href="https://github.com/DemocraciaEnRed/merepresenta-web" target="_blank">acá</a> ;). Natán Szmedra, que programó la trivia, y al equipo de Maná Digital, que hicieron el Compás Político. A Alvaro Araujo, en la parte de atrás, #imprescindible. A Diego Vilardebó, por cubrirnos la espalda. A Guillermo Croppi, por ser tan crack, Guille.
+            </p>
+            <p class="mb-4">Si a quien lee le parece que hay mucha información, que ¿cuántas variables más van a agregar?, que tal está mal, que cual está bien, que vamos 85%, es porque  Luciana Vega y Valeria Lisciani coordinaron los equipos y lo DEJARON TODO. 
+            </p>
+            <p class="mb-4">A Powa ❤️ , a Flor Caffarone, por ser tan lo más. Y a Agus Frizzera, por las dudas.
+            </p>
+            <p class="mb-4">Porque sólo seremos mejores cuando seamos capaces de soñarnos mejores. 
+
+            </p>
+        </div>
+    </section>
+</main>
+<style>
+    .logo-mere{
+        filter: invert(42%) sepia(93%) saturate(0%) hue-rotate(87deg) brightness(0%) contrast(119%);
+    }
+</style>
