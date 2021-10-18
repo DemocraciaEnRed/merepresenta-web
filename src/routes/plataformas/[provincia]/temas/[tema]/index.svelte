91b56f9b011e6e72a22a5cab8f7bea3266edbb79
Guillermo Croppi <guillermocroppi@gmail.com>
2021-10-18 11:05:08 -0300

    Deleted repeated things

--- src/routes/plataformas/[provincia]/temas/[tema]/index.svelte
+++ src/routes/plataformas/[provincia]/temas/[tema]/index.svelte
@@ -96,48 +96,6 @@
     </div>
   </div>
 </div>
-<main class="white-background-desktop">
-  <section class="container ">
-    <nav class="breadcrumb is-small pl-2 my-2" aria-label="breadcrumbs">
-      <ul>
-        <li ><a href="/plataformas/donde-votas">Plataformas</a></li>
-        <li ><a href="/plataformas/temas">Temas</a></li>
-        <li class="is-active"><a href aria-current="page" style="color:{tema.color}">{tema.name}</a></li>
-      </ul>
-    </nav>
-    <div class="has-text-right my-2">
-      <SelectDistrict/>
-    </div>
-    <div class="columns header-container mt-6 is-tablet">
-       <HeaderPP {tema}/>
-      <div class="info column  is-three-quarters-tablet p-4 general-sans">
-        <p class="has-text-weight-medium is-size-5">
-          Consultá las distintas propuestas sobre
-          <strong style="color: {tema.color};text-transform: capitalize">{tema.name}</strong> 
-          de cada partido según tu distrito.
-        </p>
-        {#if tema.slug==='economia'|| tema.slug==='ambiente'|| tema.slug==='genero' || tema.slug==='justicia' || tema.slug==='vivienda' || tema.slug==='institucionalidad'}
-          <!-- <p class="mt-2">
-            <em class="mt-6">Si querés saber sobre el estado actual de {tema.name.toLowerCase()} en la Argentina descargá nuestra hoja de diagnóstico </em>
-          </p> -->
-          <p class="has-text-centered mt-4">
-            <a href={tema.sheet_url} target="_blank" class="download-sheet">
-              <img src="/download.png" alt="icono de descarga">
-              Descargá nuestra hoja de diagnóstico
-            </a>
-          </p>
-          {/if}
-      </div>
-    </div>
-   
-    <div class="py-4 is-flex-tablet is-flex-wrap-wrap columns">
-      {#each partidos as partido}
-        <ProposalsByParty {tema} {partido}/>
-      {/each}
-    </div>
-    
-  </section>
-</main>
 
 <style>
   nav.breadcrumb .is-active{
