5ee593d223fe9e112c79a67c0b905286647c0cb6
lieta96 <julieta.garcia96@gmai.com>
2021-10-18 11:47:09 -0300

    delete logos

--- src/routes/juegos/index.svelte
+++ src/routes/juegos/index.svelte
@@ -1,15 +1,18 @@
 <script>
     import Game from './_game.svelte'
 </script>
-<main class="r black-background
-	p-2 has-background-black">
-    <section class="containe has-text-left-mobile has-text-centered-tablet container p-4 my-6">
-        <h2 class="title light is-4 has-text-weight-normal has-text-white">Desafiá todo lo que crees saber sobre la política argentina.</h2>
-    </section>
-    <section class="container p-4 has-text-centered">
-        <div class="">
-            <Game background='games/juego1.png' title="TRIVIA" subtitle="Juego de preguntas y respuestas" url={"https://trivia.merepresenta.info/"} textColor="white"/>
-            <Game background='games/juego2.png' title="PRÓXIMAMENTE" subtitle="Comás político" url={"#"} textColor="black"/>
-        </div>
-    </section>
+<main class="r hero
+	 has-background-black tetris-background">
+    <div class="hero-body p-2">
+        <section class="containe has-text-left-mobile has-text-centered-tablet container p-4 my-6">
+            <h2 class="title light is-4 has-text-weight-normal has-text-white">Desafiá todo lo que crees saber sobre la política argentina.</h2>
+        </section>
+        <section class="container p-4 has-text-centered">
+            <div class="">
+                <Game background='games/juego1.png' title="TRIVIA" subtitle="Juego de preguntas y respuestas" url={"https://trivia.merepresenta.info/"} textColor="white"/>
+                <Game background='games/juego2.png' title="PRÓXIMAMENTE" subtitle="Comás político" url={"#"} textColor="black"/>
+            </div>
+        </section>
+    </div>
+    
 </main>
