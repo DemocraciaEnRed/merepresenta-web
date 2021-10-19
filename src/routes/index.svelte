49ce29d7e49372dbd43e4e2dafc9685c06319614
lieta96 <julieta.garcia96@gmai.com>
2021-10-19 10:17:46 -0300

    ajustes home

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,17 @@
+<script context="module">
+	import API, { handleResponse } from "$lib/apiHandler";
+	import { getThemes } from "$lib/graph-ql/themes.js";
+	export async function load({fetch}){
+	  const res = await API(fetch, getThemes());
+	  return await handleResponse(res, "temas", "ejes")
+	}
+  </script>
 <script>
 	import Hero from '$lib/header/Hero.svelte'
-import { space } from 'svelte/internal';
+	import { loop_guard, space } from 'svelte/internal';
+	import { page } from "$app/stores";
+	import { directusImg, ProvinciasSlugs } from "$lib/common/utils";
+	export let temas;
 </script>
 <svelte:head>
 	<!-- <title>Home</title> -->
@@ -16,25 +27,24 @@ import { space } from 'svelte/internal';
 		<div class="hero-body">
 			<div class="container">
 				<img src="/logo-merepresenta.svg" width="400" class="image p-4 mb-5 is-hidden-desktop" alt="">
-				
 				<div class="columns is-centered is-mobile">
 					<div class="column is-8-desktop">
 						<h1 class="title is-3 is-size-4-touch has-text-centered has-text-left-touch has-text-white">
-							Te ayudamos a votar de manera informada
+							Una herramienta para pensar tu voto
 						</h1>
 						<h2 class="subtitle is-3 is-size-4-touch has-text-centered has-text-left-touch has-text-white">
-							Conocé candidaturas, partidos, plataformas electorales y muchos más
+							Conocé candidaturas, partidos, propuestas y mucho más.
 						</h2>
 					</div>
 				</div>
 				<div class="is-hidden-touch">
 					<div class="has-text-centered mt-6">
-						<h2 class="title is-1 has-text-black has-background-white p-3 px-6 is-inline-block is-uppercase">A vos, ¿Quién te representa?</h2>
+						<h2 class="title is-1 has-text-white p-3 px-6 is-inline-block is-uppercase">¿SABÉS QUIÉN TE REPRESENTA? </h2>
 						
 					</div>
 					<div class="buttons is-centered mt-6">
 						<a href="/partidos-y-candidates/donde-votas" class="animate__animated animate__pulse animate__infinite animate__slow button is-size-4 is-white is-inline is-uppercase has-text-weight-semibold py-3 px-6">
-							Ver partidos y candidaturas
+							VER CANDIDATURAS Y PARTIDOS
 					</a>
 				</div>
 			</div>
@@ -72,11 +82,19 @@ import { space } from 'svelte/internal';
 			</div>
 		</div>
 	</div>
-		<!-- Home on Tablet -->
+		<!-- Home on Desktop -->
 		<div class="section is-hidden-touch">
 			<div class="container desktop-topics has-text-centered">
-				<div class="columns is-vcentered icons mb-6">
-					<div class="column">
+				<div class="columns is-vcentered icons mb-6 is-align-items-stretch">
+
+					{#each temas as tema}
+						<div class="column has-text-centered">
+							<div class="topic-container is-flex is-justify-content-center" style="background-color: {tema.color};">
+								<img src="{directusImg}{tema.icon_file.id}" class="icon-topic"  alt="icono de {tema.name}"/>
+							</div>
+						</div>
+					{/each}
+					<!-- <div class="column">
 						<img src="themes/gender.svg" class="image mx-auto" alt="Ícono de género">
 					</div>
 					<div class="column ">
@@ -99,7 +117,7 @@ import { space } from 'svelte/internal';
 					</div>
 					<div class="column">
 						<img src="themes/security.svg" class="image mx-auto" alt="Ícono de seguruidad">
-					</div>
+					</div> -->
 				</div>
 					<h2 class="title is-1 has-text-white has-background-black p-3 px-6 is-inline-block is-uppercase">¿Queres conocer<br>las propuestas?</h2>
 					<div class="buttons is-centered">
@@ -108,16 +126,15 @@ import { space } from 'svelte/internal';
 					
 					<div class="divider"></div>
 						<img class="image mx-auto vote-logo" src="/vote.png" width="240" alt="">	
-						<h2 class="title is-1 has-text-white has-background-black p-3 px-6 is-inline-block is-uppercase on-top">¿No sabes donde se vota?</h2>
+						<h2 class="title is-1 has-text-white has-background-black p-3 px-6 is-inline-block is-uppercase on-top">¿SABÉS QUÉ SE VOTA?</h2>
 						<div class="buttons is-centered">
-							<a href="/abc-electoral" class="button is-black is-large is-uppercase has-text-weight-semibold px-6 is-outlined">ver ABC sobre las elecciones 2021</a>
+							<a href="/abc-electoral" class="button is-black is-large is-uppercase has-text-weight-semibold px-6 is-outlined">VER ABC ELECTORAL</a>
 						</div>
 						<div class="divider"></div>
 						<img class="image mx-auto games-logo" src="/juegos.svg" width="240" alt="Ícono de juegos">
-						<h2 class="title is-1 has-text-white has-background-black p-3 px-6 is-inline-block is-uppercase on-top mb-3">¡Informate jugando!</h2>
-						<p class="is-uppercase general-sans has-text-weight-medium has-text-black is-size-5">Mirá nuestros juegos online para descubrir más sobre la política argentina</p>
+						<h2 class="title is-1 has-text-white has-background-black p-3 px-6 is-inline-block is-uppercase on-top mb-3">¿SABÉS DE POLÍTICA ARGENTINA?</h2>
 						<div class="buttons is-centered mt-6">
-							<a href="/juegos" class="button is-black is-large is-uppercase has-text-weight-semibold px-6 is-outlined">ver ABC sobre las elecciones 2021</a>
+							<a href="/juegos" class="button is-black is-large is-uppercase has-text-weight-semibold px-6 is-outlined">DESAFIATE CON ESTOS JUEGOS</a>
 						</div>
 
 			<!-- <div class="divider my-6"></div> -->
@@ -173,6 +190,9 @@ import { space } from 'svelte/internal';
 	.on-top{
 		position: relative;
 		z-index: 20;
+	}
+	.topic-container{
+		height: 100%;
 	}
 
 </style>
