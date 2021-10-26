8c6d7de72f52fa05e3bda384b528090da42a153c
lieta96 <julieta.garcia96@gmai.com>
2021-10-26 13:16:10 -0300

    animacion juegos

--- src/routes/juegos/_game.svelte
+++ src/routes/juegos/_game.svelte
@@ -4,15 +4,29 @@
     export let subtitle;
     export let url;
     export let textColor;
+    export let delay
 
 </script>
 <div>
-    <a style="background-image:url({background})" class="has-background-white is-offset-one-quarter mx-auto p-5 column is-half background-trivia mb-6 is-flex is-flex-direction-column is-justify-content-center" href="{url}" target="_blank">
+    <a style="background-image:url({background})" class="{delay} has-background-white is-offset-one-quarter mx-auto p-5 column is-half background-trivia mb-6 is-flex is-flex-direction-column is-justify-content-center" href="{url}" target="_blank">
         <h1 class="is-size-3 title is-4 has-text-{textColor} has-text-weight-bold">{title}</h1>
         <span class="has-background-black"><p class="has-text-white">{subtitle}</p> </span>
     </a>
 </div>
 <style>
+    @keyframes anim{
+        0% {box-shadow: none;}
+        50% {box-shadow: 0 0 10px #ffffffcc, 0 0 21px #ffffff96, 0 0 21px #ffffff4a}
+        100%{box-shadow: none;}
+    }
+    a {
+    animation-name: anim;
+    animation-duration: 5s;
+    animation-iteration-count: infinite;
+    }
+    a.delay{
+     animation-delay:2.5s   
+    }
     a{
         background-repeat: no-repeat;
         background-size: cover;
--- src/routes/juegos/index.svelte
+++ src/routes/juegos/index.svelte
@@ -8,11 +8,11 @@
             <h2 class="title light is-4 has-text-weight-normal has-text-white">Desafiá todo lo que crees saber sobre la política argentina.</h2>
         </section>
         <section class="container p-4 has-text-centered">
-            <div class="">
-                <Game background='games/juego1.png' title="TRIVIA" subtitle="Juego de preguntas y respuestas" url={"https://trivia.merepresenta.info/"} textColor="white"/>
-                <Game background='games/juego2.png' title="COMPÁS POLÍTICO" subtitle="PRÓXIMAMENTE" url={"#"} textColor="black"/>
+            <div >
+                <Game  background='games/juego1.png' title="TRIVIA" subtitle="Juego de preguntas y respuestas" url={"https://trivia.merepresenta.info/"} textColor="white" />
+                <Game delay={'delay'}  background='games/juego2.png' title="COMPÁS POLÍTICO" subtitle="PRÓXIMAMENTE" url={"#"} textColor="black"/>
             </div>
         </section>
     </div>
-    
+ 
 </main>
