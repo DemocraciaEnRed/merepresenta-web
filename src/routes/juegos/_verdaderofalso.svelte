2eb46260e9262cb55743145287fb368f9458b59a
lieta96 <julieta.garcia96@gmai.com>
2021-11-05 12:56:51 -0300

    link verdadero o falso

--- src/routes/juegos/_verdaderofalso.svelte
+++ src/routes/juegos/_verdaderofalso.svelte
@@ -1,18 +1,17 @@
 <script>
     export let background;
     export let delay
+    export let url
 
 </script>
 <div>
-    <a style="background-image:url({background})" class="{delay} has-background-white is-offset-one-quarter mx-auto p-5 column is-half background-trivia mb-6 is-flex is-flex-direction-column is-justify-content-center" href="{null}" target="_blank">
+    <a style="background-image:url({background})" class="{delay} has-background-white is-offset-one-quarter mx-auto p-5 column is-half background-trivia mb-6 is-flex is-flex-direction-column is-justify-content-center" href="{url}" target="_blank">
         <h1 class="is-size-3 title is-4 has-text-black has-text-weight-bold">
             <span class="is-uppercase has-background-success has-text-white px-1">verdadero</span>
             <br> o <br>
             <span class="has-background-danger is-uppercase has-text-white px-1 ">falso</span>
         
         </h1>
-
-        <span class="has-background-black"><p class="has-text-white">próximamente</p> </span>
     </a>
 </div>
 <style>
--- src/routes/juegos/index.svelte
+++ src/routes/juegos/index.svelte
@@ -12,19 +12,21 @@
             <div >
                 <Game  background='games/juego1.png' title="TRIVIA" subtitle="Juego de preguntas y respuestas" url={"https://trivia.merepresenta.info/"} textColor="white" />
                 <Game delay={'delay'}  background='games/juego2.png' title="COMPÁS POLÍTICO"  subtitle="¿Cuál es tu ideología?" url={"https://compaspolitico.merepresenta.info/"} textColor="black"/>
-                <VerdaderoFalso  background='games/verdaderofalso.png' url={null} />
+                <VerdaderoFalso  background='games/verdaderofalso.png' url={"https://verdaderofalso.merepresenta.info/"} />
 
                 <div class="columns">
                     <div class="column is-half mx-auto my-6 ">
                         <h3 class="subtitle light has-text-white is-4">
-                            ¿Te gustan más los juegos de mesa?
-                            <span class="has-text-weight-medium">Descargate nuestro juego de la oca para imprimir y jugar en casa</span>
+                            ¿Preferís un juego de mesa?<br>
+                            <span class="has-text-weight-medium">Descargate nuestro Juego de la Oca
+                            </span>
                         </h3>
                     </div>
                 
                 </div>
                 <div class="column is-12">
-                    <Game delay={'delay'} background='games/oca.png' title="Juego de la oca" subtitle="*incluye tablero y caja para imprimir, e instrucciones de uso" url={"https://drive.google.com/drive/folders/1u26igX-aGzXn9CnU1tJ5JI35S8SUKZ1t"} textColor="white"/>
+                    <Game delay={'delay'} background='games/oca.png' title="Juego de la oca" subtitle="descargá las instrucciones para imprimir, el tablero y la caja
+                    " url={"https://drive.google.com/drive/folders/1u26igX-aGzXn9CnU1tJ5JI35S8SUKZ1t"} textColor="white"/>
                 </div>
             </div>
         </section>
