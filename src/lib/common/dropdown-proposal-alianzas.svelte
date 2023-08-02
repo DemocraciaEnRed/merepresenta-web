be2a920c36d42874352e3ea8e5c674fc926336e0
xtian7489 <xtian7489@gmail.com>
2023-08-02 09:19:14 -0300

    agregando mostrar propuesta completa en perfil del partido alianza

--- src/lib/common/dropdown-proposal-alianzas.svelte
+++ src/lib/common/dropdown-proposal-alianzas.svelte
@@ -8,19 +8,21 @@
 	import { PoliciesIcons } from './utils';
 	export let darkMode;
 	let color = darkMode ? 'white' : 'black';
-	export let backgroundHeader;
-	let backgroundHeaderColor = backgroundHeader ? 'black' : '';
-	let borderSolid = backgroundHeader ? 'border-solid p-2' : '';
+	$: showCompleteProposal = [];
+	const addToShowComplete = (alianzaId) => {
+		const index = showCompleteProposal.findIndex((el) => el === alianzaId);
+		if (index < 0) showCompleteProposal = [...showCompleteProposal, alianzaId];
+		else showCompleteProposal = showCompleteProposal.filter((el) => el !== alianzaId);
+	};
 </script>
 
 <div class="the-drop-header {!isOpen ? 'border-radius' : ''} drop" style="border-color: {color};">
-	<div
+	<button
 		id="partido-{proposal.ejes_id.slug}"
-		class="proposal-header is-flex is-flex-direction-row is-align-items-center  has-background-white "
-		
+		class="proposal-header is-flex is-flex-direction-row is-align-items-center has-background-white p-0"
 		on:click={() => (isOpen = !isOpen)}
 	>
-		<div class="icon-container " style="background-color: {proposal.ejes_id.color};">
+		<div class="icon-container" style="background-color: {proposal.ejes_id.color};">
 			<img
 				src={PoliciesIcons[proposal.ejes_id.slug]}
 				class="image m-3 icon-proposal"
@@ -38,21 +40,47 @@
 			</div>
 		</div>
 		<span class="pr-6" style="color:{color}"><Icon icon={iconClass} /></span>
-	</div>
+	</button>
 	{#if isOpen}
 		<div class="proposal-body summary general-sans" transition:slide>
 			<div class="">
 				<div class=" has-text-centered-touch has-text-left-desktop">
 					{#each alianzas as alianza}
-					{#each alianza.ejes as proposalAlianza}
-					{#if proposalAlianza.ejes_id.slug === proposal.ejes_id.slug}
-					<div class="alianza-proposal p-5">
+						{#each alianza.ejes as proposalAlianza}
+							{#if proposalAlianza.ejes_id.slug === proposal.ejes_id.slug}
+								<div class="alianza-proposal p-5">
 									<h1 class="has-text-weight-bold is-uppercase mb-2 has-text-black">
 										Lista: {alianza.name}
 									</h1>
-									<p>{proposalAlianza.summary}</p>
+									{#if !showCompleteProposal.includes(alianza.id)}
+										<p>{proposalAlianza.summary}</p>
+									{/if}
+									{#if showCompleteProposal.includes(alianza.id)}
+										<div class="column">
+											{#each proposalAlianza.propuestas as item, i}
+												<div class="my-4 has-text-centered-touch has-text-left-desktop">
+													<h1
+														class="has-text-weight-bold is-uppercase mb-2 has-text-black is-inline mr-1"
+													>
+														Propuesta {i + 1}:
+													</h1>
+													<p class="is-inline">{item.summary}</p>
+												</div>
+											{/each}
+										</div>
+									{/if}
+									<button
+										class="button is-link is-rounded is-uppercase has-text-weight-semibold mt-4 source-button"
+										on:click={() => addToShowComplete(alianza.id)}
+									>
+										<u
+											>{!showCompleteProposal.includes(alianza.id)
+												? 'propuesta completa'
+												: 'propuesta resumida'}</u
+										>
+									</button>
 									{#if alianza.url_fuente}
-										<p>
+										<p class="is-inline ml-2">
 											<a
 												href={alianza.url_fuente}
 												target="_blank"
@@ -62,8 +90,8 @@
 										</p>
 									{/if}
 								</div>
-								{/if}
-							{/each}
+							{/if}
+						{/each}
 					{/each}
 				</div>
 			</div>
@@ -92,6 +120,7 @@
 		border: 2px solid black;
 		width: 100%;
 		margin-top: 1rem;
+		font-family: 'Nippo', Helvetica, Arial, sans-serif;
 	}
 	.proposal-body {
 		width: 100%;
